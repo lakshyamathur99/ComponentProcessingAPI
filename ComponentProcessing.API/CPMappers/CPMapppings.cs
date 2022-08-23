2d415fe41ea03f39aecef5d0290e07f7e0ea81ef
lakshyamathur99 <lakshyamathur00@gmail.com>
2022-08-23 14:12:56 +0530

    ROM

---
+++ ComponentProcessing.API/CPMappers/CPMapppings.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ComponentProcessing.API.Models;
+using ComponentProcessing.API.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComponentProcessing.API.CPMappers
+{
+    public class CPMapppings: Profile
+    {
+        public CPMapppings()
+        {
+            CreateMap<ComponentProcessingModel, ComponentProcessingModelDto>().ReverseMap();
+            CreateMap<ComponentProcessingModel, CreateDto>().ReverseMap();
+            CreateMap<ComponentProcessingModel, UpdateDto>().ReverseMap();
+        }
+    }
+}
